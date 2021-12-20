3f04aa401eb74724b6d9b1734af2baa173baaebf
AntonOshurek <oshurekweb@gmail.com>
2021-12-20 22:14:36 +0100

    add caps lock on off logick

--- source/js/script.js
+++ source/js/script.js
@@ -1,15 +1,39 @@
 const showPassButton = document.querySelector('.login-form__show-password');
 const passwordButton = document.querySelector('.login-form__input--password');
-const showIcon = document.querySelector('.closed-eye');
+const closedEye = document.querySelector('.closed-eye');
+const closedIcon = document.querySelector('.closed-icon');
+
+const caps = document.querySelector('.caps');
 
 showPassButton.addEventListener('click', (evt) => {
   evt.preventDefault();
   if(passwordButton.type === 'password') {
     passwordButton.type = 'text';
-    showIcon.classList.add('closed-eye--open');
+    closedEye.classList.add('closed-eye--open');
+    closedIcon.classList.add('closed-icon--open');
   } else {
     passwordButton.type = 'password';
-    showIcon.classList.remove('closed-eye--open');
+    closedEye.classList.remove('closed-eye--open');
+    closedIcon.classList.remove('closed-icon--open');
   }
 });
 
+document.addEventListener('keydown', (evt) => {
+  const capsOn = evt.getModifierState && evt.getModifierState('CapsLock');
+  if(evt.target.classList.contains('login-form__input')) {
+    if(capsOn) {
+      const activePlace = document.activeElement.getBoundingClientRect();
+      caps.classList.add('caps--open');
+      caps.style.top = `${activePlace.top}px`;
+      caps.style.left = `${activePlace.left}px`;
+    } else {
+      caps.style.top = '';
+      caps.style.left = '';
+      caps.classList.remove('caps--open');
+    }
+  } else {
+    caps.style.top = '';
+    caps.style.left = '';
+    caps.classList.remove('caps--open');
+  }
+});
